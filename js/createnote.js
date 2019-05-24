ec3438c4f8b3a4ca0d23e114a9b66613f9dbd7c7
Mario Knecht <mario.knecht@gmail.com>
2019-05-24 20:35:53 +0200

    add filter and ordering

--- js/createnote.js
+++ js/createnote.js
@@ -14,13 +14,13 @@ function getNote() {
     let finished = false;
     const newNote = {};
 
+    newNote['id'] = createNewId();
     newNote[document.querySelector('#title').id] = document.querySelector('#title').value;
     newNote[document.querySelector('#description').id] = document.querySelector('#description').value;
     newNote[document.querySelector('input[name="importance"]:checked').name] = document.querySelector('input[name="importance"]:checked').value;
-    newNote[document.querySelector('#donedate').id] = document.querySelector('#donedate').value;
+    newNote[document.querySelector('#donedate').id] = new Date(document.querySelector('#donedate').value);
     newNote['createDate'] = Date.now(); //format, use moments.js?
     newNote['finished'] = finished;
-
     return newNote
 }
 
@@ -31,4 +31,10 @@ function save() {
     notes.push(getNote());
     localStorage.setItem('notes', JSON.stringify(notes));
     window.location.replace("index.html");
+}
+
+function createNewId() {
+    let notes = JSON.parse(localStorage.getItem('notes'));
+    notes = notes.length + 1;
+    return notes.toString();
 }
--- js/notelist.js
+++ js/notelist.js
@@ -4,59 +4,65 @@
  * array of notes (static)
  */
 
-const notes = [
+const notesOrigin = [
     {
-        "id": "1",
-        "title": "Milch holen",
-        "description": "Bitte einen Liter Milch in der Migros holen",
-        "donedate": "2019-06-01",
-        "importance": "1",
-        "finished": false
+        id: "1",
+        title: "Milch holen",
+        description: "Bitte einen Liter Milch in der Migros holen",
+        donedate: "2019-06-01T00:00:00.000Z",
+        createdate: "1558716364511",
+        importance: "1",
+        finishe: false
     },
     {
-        "id": "2",
-        "title": "Butter holen",
-        "description": "Bitte einen Butter in der Migros holen",
-        "donedate": "2019-06-01",
-        "importance": "1",
-        "finished": false
+        id: "2",
+        title: "Butter holen",
+        description: "Bitte einen Butter in der Migros holen",
+        donedate: "2019-04-01T00:00:00.000Z",
+        createdate: "1358716364511",
+        importance: "1",
+        finished: false
     },
     {
-        "id": "3",
-        "title": "Äpfel holen",
-        "description": "Saisonale Sorte berücksichtigen",
-        "donedate": "2019-06-20",
-        "importance": "5",
-        "finished": false
+        id: "3",
+        title: "Äpfel holen",
+        description: "Saisonale Sorte berücksichtigen",
+        donedate: "2019-02-20T00:00:00.000Z",
+        createdate: "1158716364511",
+        importance: "5",
+        finished: true
     },
     {
-        "id": "4",
-        "title": "Termin Wanderung Nicole",
-        "description": "Bleiben wir in der Schweiz oder gehen wir weiter weg?",
-        "donedate": "2019-07-11",
-        "importance": "3",
-        "finished": false
+        id: "4",
+        title: "Termin Wanderung Nicole",
+        description: "Bleiben wir in der Schweiz oder gehen wir weiter weg?",
+        donedate: "2019-07-11T00:00:00.000Z",
+        createdate: "1518716364511",
+        importance: "3",
+        finished: false
     },
     {
-        "id": "5",
-        "title": "Pneuwechsel",
-        "description": "Termin mit Garage vereinbaren",
-        "donedate": "2019-08-14",
-        "importance": "2",
-        "finished": false
+        id: "5",
+        title: "Pneuwechsel",
+        description: "Termin mit Garage vereinbaren",
+        donedate: "2019-08-14T00:00:00.000Z",
+        createdate: "2558716364511",
+        importance: "2",
+        finished: false
     },
     {
-        "id": "6",
-        "title": "Tickets Open Air",
-        "description": "René fragen ob er bereits Tickets organisiert hat?",
-        "donedate": "2019-09-02",
-        "importance": "4",
-        "finished": false
+        id: "6",
+        title: "Tickets Open Air",
+        description: "René fragen ob er bereits Tickets organisiert hat?",
+        donedate: "2019-09-02T00:00:00.000Z",
+        createdate: "1958716364511",
+        importance: "4",
+        finished: false
     }
 ];
 const templateSource = document.querySelector('#note-item-template').innerHTML;
 const template = Handlebars.compile(templateSource);
-const filteredNotes = [];
+let notesFiltered = [];
 
 /**
  * initialize application
@@ -65,32 +71,49 @@ const filteredNotes = [];
 init();
 
 function init() {
-    renderNotes();
+    notesFiltered = [...notesOrigin];
+    renderNotes(notesFiltered);
     setEventListeners();
 }
 
 function setEventListeners() {
-    document.querySelector('#filter-finish-date').addEventListener('click',orderByFinishDate);
-    document.querySelector('#filter-create-date').addEventListener('click',orderByCreateDate);
-    document.querySelector('#filter-importance').addEventListener('click',orderByImportance);
-    document.querySelector('#filter-finished').addEventListener('click',showFinished);
+    document.querySelector('#filter-finish-date').addEventListener('click', orderByFinishDate);
+    document.querySelector('#filter-create-date').addEventListener('click', orderByCreateDate);
+    document.querySelector('#filter-importance').addEventListener('click', orderByImportance);
+    document.querySelector('#filter-finished').addEventListener('click', showFinished);
 }
 
 function orderByFinishDate() {
-    console.log('this is the finish date btn');
+    notesFiltered = [...notesOrigin].sort(function (a, b) {
+        let dateA = new Date(a.donedate), dateB = new Date(b.donedate);
 
+        return dateA - dateB;   //nearest date
+    });
+
+    renderNotes(notesFiltered);
 }
 
 function orderByCreateDate() {
-    console.log('this is the create date btn');
+    notesFiltered = [...notesOrigin].sort(function (a, b) {
+        let dateA = a.createdate, dateB = b.createdate;
+        return dateA - dateB;   //nearest date
+    });
+
+    renderNotes(notesFiltered);
 }
 
 function orderByImportance() {
-    console.log('this is the importance btn');
+    notesFiltered = [...notesOrigin].sort(function (a, b) {
+        let importanceA = a.importance, importanceB = b.importance;
+        return Number(importanceB) - Number(importanceA); //most important on top
+    });
+
+    renderNotes(notesFiltered);
 }
 
 function showFinished() {
-    console.log('this is the finished btn');
+    notesFiltered = [...notesOrigin].filter(note => note.finished === true);
+    renderNotes(notesFiltered);
 }
 
 /**
@@ -99,7 +122,7 @@ function showFinished() {
  */
 
 
-function renderNotes() {
+function renderNotes(notes) {
     const noteContainer = document.querySelector('#list-container');
     noteContainer.innerHTML = template(notes);
 }
@@ -112,7 +135,7 @@ function renderNotes() {
 
 let storedNotes = localStorage.getItem('notes');
 if (!storedNotes) {
-    localStorage.setItem('notes', JSON.stringify([]));
+    localStorage.setItem('notes', JSON.stringify(notesOrigin)); //set static array from this file
     storedNotes = localStorage.getItem('notes');
 }
 
