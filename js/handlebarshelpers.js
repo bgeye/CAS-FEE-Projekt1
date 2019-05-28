860803643fd37128a7731ed2472309f732705478
Mario Knecht <mario.knecht@gmail.com>
2019-05-28 23:22:03 +0200

    add handlebar-helper for checkbox

--- js/handlebarshelpers.js
+++ js/handlebarshelpers.js
@@ -15,6 +15,23 @@ Handlebars.registerHelper('checkImportance', function(value,importance) {
 });
 
 /**
+ * Helper to check if note has state finished
+ */
+
+Handlebars.registerHelper('isFinished', function(value) {
+
+
+    if(value === true) {
+        return 'checked'
+    } else {
+        return ''
+    }
+
+    //value => (value === importance) ? 'checked' : '';
+
+});
+
+/**
  * Helper to format the date for the datepicker in editform
  */
 Handlebars.registerHelper('formatDateDatepicker', function(date) {
